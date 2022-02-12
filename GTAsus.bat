f6344a3d65622c7a3979304a4f753613d2424cef
Matt <46137770+xMdb@users.noreply.github.com>
2022-02-12 07:16:31 +0000

    Add some cool ASCII art

--- GTAsus.bat
+++ GTAsus.bat
@@ -1,11 +1,24 @@
 @echo off
+color b
+echo  ::::::::  :::    :::  ::::::::   ::::::::  :::   ::: 
+echo :+:    :+: :+:    :+: :+:    :+: :+:    :+: :+:   :+: 
+echo +:+        +:+    +:+ +:+        +:+         +:+ +:+  
+echo +#++:++#++ +#+    +:+ +#++:++#++ +#++:++#++   +#++:   
+echo        +#+ +#+    +#+        +#+        +#+    +#+    
+echo #+#    #+# #+#    #+# #+#    #+# #+#    #+#    #+#    
+echo  ########   ########   ########   ########     ###     
+echo.
+echo Just a useful tool by xMdb. 
+timeout 2 /NOBREAK >nul
 color c
+echo.
+echo.
 echo Kicking all users...
-pssuspend64 GTA5.exe
+"D:\Files\Programs\Other Programs\GTAsus\pssuspend64.exe" GTA5.exe
 echo Waiting 12 seconds...
 timeout 12 /NOBREAK >nul
-pssuspend64 -r GTA5.exe
-echo Success!
+"D:\Files\Programs\Other Programs\GTAsus\pssuspend64.exe" -r GTA5.exe
+echo Success! Enjoy your lobby free of griefers.
 color a
 timeout 2 /NOBREAK >nul
 exit
